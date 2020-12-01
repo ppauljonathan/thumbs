5eede98f9ae294b3f15e64e0b88c833e81459ff0
P Paul Jonathan <70207656+ppauljonathan@users.noreply.github.com>
2020-12-01 11:56:39 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from tkinter import Tk,Button,PhotoImage,Toplevel
+window=Tk()
+window.geometry("200x100")
+window.resizable(False,False)
+
+
+def tu():
+    img=Toplevel(window)
+    img.title("ex")
+    img.geometry("100x100")
+    img.resizable(False,False)
+    bt=Button(img,image=a).pack(side="left")
+
+
+def td():
+    img=Toplevel(window)
+    img.title("ex")
+    img.geometry("100x100")
+    img.resizable(False,False)
+    bt=Button(img,image=b).pack(side="left")
+
+a=PhotoImage(file="./a.png")
+Button(window,image=a,command=tu).pack(side="left")
+
+b=PhotoImage(file="./b.png")
+Button(window,image=b,command=td).pack(side="right")
+
+window.mainloop()
